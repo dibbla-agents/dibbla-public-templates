a64901d152f04144ce01475ee5829c9a43bc8065
Joakim Edlund <joakim@dibbla.com>
2026-03-12 09:44:11 +0100

    Update hello world template

--- getting-started-1/frontend/src/vite-env.d.ts
+++ getting-started-1/frontend/src/vite-env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="vite/client" />
+
+declare const __PROJECT_PATH__: string;
--- getting-started-1/frontend/vite.config.ts
+++ getting-started-1/frontend/vite.config.ts
@@ -1,9 +1,13 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  define: {
+    __PROJECT_PATH__: JSON.stringify(path.resolve(process.cwd(), "..")),
+  },
   build: {
     outDir: "../dist",
     emptyOutDir: true,
